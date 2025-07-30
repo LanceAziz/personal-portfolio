ce75544dbace188ba775e762ca51677d46ad8d88
LanceAziz <lanceaziz2002@gmail.com>
2025-07-30 10:09:59 +0300

    refactor: streamline heading component and update link handling

--- app/utils/text.js
+++ app/utils/text.js
@@ -1,19 +1,28 @@
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faInbox } from '@fortawesome/free-solid-svg-icons';
+
+
 export const text = {
     heading: {
         photo: "/images/personal-photo.jpg",
         name: "Lance Aziz",
         title: "Full Stack Developer",
-        email: {
-            label: "Email",
-            url: "lancemohebsamir@gmail.com"
-        },
-        github: {
-            label: "Github",
-            url: "https://github.com/LanceAziz/"
-        },
-        linkedin: {
-            label: "Linkedin",
-            url: "https://www.linkedin.com/in/lance-aziz-316945316/"
-        },
+        links: [
+            {
+                icon: faInbox,
+                label: "Email",
+                url: "lancemohebsamir@gmail.com",
+            },
+            {
+                icon: faGithub,
+                label: "Github",
+                url: "https://github.com/LanceAziz/",
+            },
+            {
+                icon: faLinkedin,
+                label: "Linkedin",
+                url: "https://www.linkedin.com/in/lance-aziz-316945316/",
+            },
+        ]
     },
 }
