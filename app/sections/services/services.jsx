976863aed4a1f08c4f281a69dffbb15f67603d23
LanceAziz <lanceaziz2002@gmail.com>
2025-08-04 09:38:33 +0300

    services v2

--- app/sections/services/services.jsx
+++ app/sections/services/services.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@/app/components/header/header.jsx'
 import { text } from '../../utils/data.js'
 import { styles } from './servicesStyles.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Services() {
     return (
@@ -11,15 +12,16 @@ export default function Services() {
                 {
                     text.sevices.list.map((service, index) => (
                         <div className="col-md-4" key={index}>
-                            <div className="service-item">
-                                <i className={`fa ${service.icon} fa-2x`}></i>
-                                <h3>{service.title}</h3>
-                                <p>{service.Description}</p>
+                            <div className="d-flex flex-column align-items-center justify-content-center text-center p-3 gap-3 bg-white text-dark rounded-4 mb-3">
+                                <FontAwesomeIcon icon={service.icon} size='2x' />
+                                <h3 className='fs-6 fw-bold'>{service.title}</h3>
+                                <div className='w-100'>
+                                    <p className='text-justify'>{service.Description}</p>
+                                </div>
+
                             </div>
                         </div>
-                    )
-                    )
-                }
+                    ))}
             </div>
         </div>
     )
