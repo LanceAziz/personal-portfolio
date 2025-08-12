921f6a15b2760591f84adb6338c735eac59547db
lance aziz mac <lance.aziz@cypodsolutions.com>
2025-08-12 10:17:57 +0300

    chnage

--- app/components/form/form.jsx
+++ app/components/form/form.jsx
@@ -5,16 +5,16 @@ export default function Form() {
     <div>
         <div className="row">
             <div className="col-6">
-                <input type="text" className="form-control mb-2 rounded-5 focus-ring focus-ring-light" placeholder="Name" />
+                <input type="text" style={styles.input} placeholder="Name" />
             </div>
             <div className="col-6">
-                <input type="text" className="form-control mb-2 rounded-5 focus-ring focus-ring-light" placeholder="Email" />
+                <input type="text" style={styles.input} placeholder="Email" />
             </div>
             <div className="col-12">
-                <input type="text" className="form-control mb-2 rounded-5 focus-ring focus-ring-light" placeholder="Subject" />
+                <input type="text" style={styles.input} placeholder="Subject" />
             </div>
             <div className="col-12">
-                <textarea rows={4} className="form-control mb-2 rounded-4 focus-ring focus-ring-light" placeholder="Message" />
+                <textarea rows={4} style={styles.input} placeholder="Message" />
             </div>
         </div>
     </div>
--- app/components/search/search.jsx
+++ app/components/search/search.jsx
@@ -1,10 +1,11 @@
 import { useProjects } from "@/app/context/projectsContext";
+import { styles } from "./searchStyles";
 
 export default function Search() {
     const { searchTerm, setSearchTerm } = useProjects();
     return (
         <div className="col-lg-3 d-flex align-items-center p-1">
-            <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className='w-100 rounded-4 px-3 py-1 border-0' placeholder='Search' type="text" />
+            <input value={searchTerm} style={styles.input} onChange={(e) => setSearchTerm(e.target.value)} className='w-100 rounded-4 px-3 py-1 border-0' placeholder='Search' type="text" />
         </div>
     )
 }
