b03c3f4574c679fe5df1db7b87084ae528c2f688
Lance Aziz <115536960+LanceAziz@users.noreply.github.com>
2026-04-27 12:18:42 +0300

    Update footer copyright year to 2026

--- app/(sections)/footer/footer.jsx
+++ app/(sections)/footer/footer.jsx
@@ -3,7 +3,7 @@ import { styles } from "./footerStyles"
 export default function Footer() {
     return (
         <div style={styles.container}>
-            <span>© 2025 Lance Aziz. All rights reserved.</span>
+            <span>© 2026 Lance Aziz. All rights reserved.</span>
         </div>
     )
 }
